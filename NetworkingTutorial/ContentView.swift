89993ba915942a817715197a094a724a8aa535cf
Ritik <ritik.k.1404@gmail.com>
2025-01-17 12:41:25 +0530

    Initial Commit

---
+++ NetworkingTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingTutorial
+//
+//  Created by Ritik Karma on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Ritik Karma on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
